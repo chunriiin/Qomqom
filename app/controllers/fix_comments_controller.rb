d00fbc66167da4aaec9951fb7aa7a419c2552e10
Pensk <pensk@hotmail.ca>
2018-12-30 15:30:42 +0900

    Allow users to make fix comments

--- app/controllers/fix_comments_controller.rb
+++ app/controllers/fix_comments_controller.rb
@@ -1,7 +1,21 @@
 class FixCommentsController < ApplicationController
+  def create
+    @comment = Comment.find(params[:comment_id])
+    @fix_comment = @comment.fix_comments.new(fix_comment_params)
+    @fix_comment.user = current_user
+    @fix_comment.save
+    redirect_to @comment.post
+  end
+
   def new
     @comment = Comment.find(params[:comment_id])
     @fix_comment = @comment.fix_comments.new
     @fix_comment.body = @comment.body
+  end
+
+  private
+
+  def fix_comment_params
+    params.require(:fix_comment).permit(:body)
   end
 end
