a2d412feabf7396e7c543b746aeef5c976333c91
Pensk <pensk@hotmail.ca>
2018-12-30 14:43:40 +0900

    Remove unnecessary variables from users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @post = Post.find(params[:id])
-    @community = Community.first
-    @comment = Comment.new
   end
 
 
